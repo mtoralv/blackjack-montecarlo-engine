cb70c127ee34400f78c0277fdbb2940f152fff74
Marcos Torres Álvarez <211049417+mtoralv@users.noreply.github.com>
2026-02-23 18:52:14 +0100

    Added suit and rank(Unfinished) files

---
+++ src/rank.java
@@ -0,0 +1,10 @@
+
+enum Rank{
+    Ace(11), Two(2), Three(3), Four(4), Five(5), Six(6), Seven(7), Eight(8), Nine(9), Ten(10), Jack(10), Queen(10), King(10);
+
+    int Value;
+    Rank(int Value)
+    {
+        this.Value = Value;
+    }
+}
