85d2086467e764a8c7370543b93b9d75b32a457c
Marcos Torres Álvarez <211049417+mtoralv@users.noreply.github.com>
2026-02-24 13:24:32 +0100

    Added Hand (Logic UNFINISHED)

---
+++ src/Hand.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class Hand {
+    
+    public ArrayList<Card> hand = new ArrayList<>();
+
+    public Hand()
+    {
+        hand=null;
+    }
+
+    public void addCard(Card card)
+    {
+        hand.add(card);
+    }
+
+    public int getValue()
+    {
+        for(Card)
+    }
+}
